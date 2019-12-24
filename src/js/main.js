b10c717eab9bbc32b2bceecd251fe4ea4070e8ac
Dmitrii Matveev <matveyeff@mail.ru>
2019-12-24 20:35:44 +0300

    slider-dots added

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+
   $('.header__slider').slick({
 		infinite: true,
 		fade: true,
 		cssEase: 'linear',
 		prevArrow: '<svg class="arrow arrow_left slider-arrows"><use xlink:href="./img/sprite.svg#arrow-right"></use></svg></a>',
-		nextArrow: '<svg class="arrow slider-arrows slider-arrows_next"><use xlink:href="./img/sprite.svg#arrow-right"></use></svg></a>'
-  });
+		nextArrow: '<svg class="arrow slider-arrows slider-arrows_next"><use xlink:href="./img/sprite.svg#arrow-right"></use></svg></a>',
+		asNavFor: '.slider-dots'
+	});
+
+	$('.slider-dots').slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		asNavFor: '.header__slider',
+	});
+
 });
