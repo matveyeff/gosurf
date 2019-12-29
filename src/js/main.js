bb57337d6d2e0f8ca41068437a81aedbbb5889ca
Dmitrii Matveev <matveyeff@mail.ru>
2019-12-30 01:03:38 +0300

    added surf-slider

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,11 @@ $(document).ready(function(){
 		asNavFor: '.header__slider',
 	});
 
+	$('.surf-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<svg class="arrow arrow_left slider-arrows"><use xlink:href="./img/sprite.svg#arrow-right"></use></svg></a>',
+		nextArrow: '<svg class="arrow slider-arrows slider-arrows_next"><use xlink:href="./img/sprite.svg#arrow-right"></use></svg></a>',
+	})
+
 });
